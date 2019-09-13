6102763655410282c31b5488443ca85cbfbfd082
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-09-13 16:05:57 +0300

    build inner structure of controllers

---
+++ controllers/order.js
@@ -0,0 +1,7 @@
+module.exports.getAll = function (req, res) {
+
+}
+
+module.exports.create = function (req, res) {
+
+}
---
+++ controllers/position.js
@@ -0,0 +1,15 @@
+module.exports.getByCategoryId = function (req, res) {
+
+}
+
+module.exports.create = function (req, res) {
+
+}
+
+module.exports.remove = function (req, res) {
+
+}
+
+module.exports.update = function (req, res) {
+
+}
