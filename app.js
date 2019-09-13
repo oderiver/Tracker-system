ccd7bdba899260d1aaeb21ec8907d7bcbd360f65
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-09-13 16:12:13 +0300

    configure app file

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express'); //упрощает разработку back-end (маршруты, mvc)
 const authRoutes = require('./routes/auth');
+const analyticsRoutes = require('./routes/analytics');
+const categoryRoutes = require('./routes/category');
+const orderRoutes = require('./routes/order');
+const positionRoutes = require('./routes/postition');
 const app = express();
 
 app.use('/api/auth/', authRoutes)
+app.use('/api/analytics/', analyticsRoutes)
+app.use('/api/category/', categoryRoutes)
+app.use('/api/order/', orderRoutes)
+app.use('/api/position/', positionRoutes)
+
 
 module.exports = app;
