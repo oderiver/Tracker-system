0d034589b48f5d0cc9edb28280d2c782a21de9ac
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-09-14 11:28:51 +0300

    handler registation & config permission

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
+
 //выполнение в случае опред роутов
 module.exports.login = function (req, res) {
     res.status(200).json({
@@ -8,8 +11,30 @@ module.exports.login = function (req, res) {
     })
 };
 
-module.exports.register = function (req, res) {
-    res.status(200).json({
-        register: 'from controller'
-    })
-};
+module.exports.register = async function (req, res) {
+    const candidate = await User.findOne({email: req.body.email})   //поиск совпадения
+
+    if(candidate){
+        res.status(409).json({
+            message: 'Такой пользователь зарегистрирован'
+        })
+    } else {
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.passwords
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch (e) {
+
+        }
+    }
+
+
+}
+
+//    user.save().then(() => console.log('User created'))
+//};
