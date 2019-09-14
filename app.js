a6f5b93ef980b70f2bac749878128475aa2b9b3a
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-09-14 17:05:15 +0300

    route protection: add passport-jwt

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')  //упрощает разработку back-end (маршруты, mvc)
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 //const cors = require('coes')    //если клиент находится на другом доменне, то мы все равно сможем отвечать нашим серверам
 //const morgan = require('morgan')    //красивое логирование запросов(то что происходит с сервером в данный момент)
 const authRoutes = require('./routes/auth');
@@ -14,6 +15,9 @@ const app = express();
 mongoose.connect(keys.mongoURI)
     .then(() => console.log('MongoDB connected.'))
     .catch(error  => console.log(error))
+
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
 app.use(require('cors')())
 app.use(bodyParser.urlencoded({extended: true}))
