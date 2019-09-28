264e03a50a268c4c9b7794112cb18226dfc2d203
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-09-28 19:36:09 +0300

    create routing for our app

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginPageComponent} from "./login-page/login-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'login', component: LoginPageComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
