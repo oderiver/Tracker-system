c6800de57d220802604340a303ce6e68fa46e5ac
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-09-28 12:14:55 +0300

    handler position crud in project

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,4 @@
 module.exports = {
     mongoURI: "mongodb+srv://tracker-system:070697world@cluster0-p8uf7.mongodb.net/test?retryWrites=true&w=majority",
-    //mongoURI: "mongodb+srv://tracker-system:070697world@cluster0-p8uf7.mongodb.net/test?retryWrites=true&w=majority",
-    //mongoURI: "mongodb://tracker-system:070697world@cluster0-shard-00-00-p8uf7.mongodb.net:27017,cluster0-shard-00-01-p8uf7.mongodb.net:27017,cluster0-shard-00-02-p8uf7.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
     jwt: 'dev-jwt'
 }
--- routes/category.js
+++ routes/category.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/category');
 const passport = require('passport')
 const router = express.Router();
 
-router.get('/', /*passport.authenticate('jwt', {session: false}), */controller.getAll)
+router.get('/', passport.authenticate('jwt', {session: false}), controller.getAll)
 router.get('/:id', controller.getById)
 router.delete('/:id', controller.remove)
 router.post('/', controller.create)
