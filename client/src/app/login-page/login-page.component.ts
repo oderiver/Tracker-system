e665863096324fdeb9c9e46d625583beaf4597ae
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-09-29 17:09:00 +0300

    write handler surround 401 error

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -32,6 +32,8 @@ export class LoginPageComponent implements OnInit, OnDestroy {
         MaterialService.toast('Теперь вы можете войти в систему используя свои данные')
       } else if(params['accessDenied']){
         MaterialService.toast('Для начала авторизируйтесь в системе')
+      } else if (params['sessionFailed']){
+        MaterialService.toast('Пожалуйста, войдите в систему заново')
       }
     })
   }
--- client/src/app/shared/classes/token.interceptor.ts
+++ client/src/app/shared/classes/token.interceptor.ts
@@ -1,12 +1,14 @@
 import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
 import {AuthService} from "../services/auth.service";
-import {Observable} from "rxjs";
+import {Observable, throwError} from "rxjs";
 import {staticViewQueryIds} from "@angular/compiler";
+import {catchError} from "rxjs/operators";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor{
-  constructor(private auth: AuthService){
+  constructor(private auth: AuthService, private router: Router){
 
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -17,6 +19,21 @@ export class TokenInterceptor implements HttpInterceptor{
         }
       })
     }
-  return next.handle(req)
+  return next.handle(req).pipe(
+    catchError(
+      (error: HttpErrorResponse) => this.handleAuthError(error)
+    )
+  )
+  }
+  private handleAuthError(error: HttpErrorResponse): Observable<any>{
+    if(error.status === 401){
+      this.router.navigate(['/login'], {
+        queryParams: {
+          sessionFiled: true
+        }
+      })
+    }
+
+    return throwError(error)
   }
 }
