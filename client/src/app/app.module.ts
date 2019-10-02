8cb4e344d8685e7b5f3c18da6778f2209200a411
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-10-02 23:26:00 +0300

    working with tooltip for fiilter date

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -20,6 +20,8 @@ import { CategoriesFormComponent } from './categories-page/categories-form/categ
 import { PositionsFormComponent } from './categories-page/categories-form/positions-form/positions-form.component';
 import { OrderCategoriesComponent } from './order-page/order-categories/order-categories.component';
 import { OrderPositionsComponent } from './order-page/order-positions/order-positions.component';
+import { HistoryListComponent } from './history-page/history-list/history-list.component';
+import { HistoryFilterComponent } from './history-page/history-filter/history-filter.component';
 
 @NgModule({
   declarations: [
@@ -37,7 +39,9 @@ import { OrderPositionsComponent } from './order-page/order-positions/order-posi
     CategoriesFormComponent,
     PositionsFormComponent,
     OrderCategoriesComponent,
-    OrderPositionsComponent
+    OrderPositionsComponent,
+    HistoryListComponent,
+    HistoryFilterComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
+
+  @ViewChild('tooltip', {static: false})tooltipRef : ElementRef
+  tooltip: MaterialInstance
+  isFilterVisible = false
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+    this.tooltip.destroy()
+  }
+
+  ngAfterViewInit(): void {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef)
+  }
 }
