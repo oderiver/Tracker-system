a049270627d8e6ba60b48fa98ef6056a47f630a1
Miroslav Ganevich <miroslavadmiral@gmail.com>
2019-10-04 16:08:45 +0300

    working with overview page

--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Observable} from "rxjs";
+import {OverviewPage} from "../shared/interfaces";
+import {AnalyticsService} from "../shared/services/analytics.service";
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
 
 @Component({
   selector: 'app-overview-page',
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.css']
 })
-export class OverviewPageComponent implements OnInit {
+export class OverviewPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('tapTarget', {static: false}) tapTargetRef : ElementRef
+  tapTarget: MaterialInstance
+  data$: Observable<OverviewPage>
+  yesterday = new Date()
+
+  constructor(private service: AnalyticsService) { }
 
   ngOnInit() {
+    this.data$ = this.service.getOverview()
+
+    this.yesterday.setDate(this.yesterday.getDate() - 1)
   }
 
+  ngAfterViewInit(): void {
+    //this.tapTarget = MaterialService.initTapTarget(this.tapTargetRef)
+  }
+
+  ngOnDestroy(): void {
+    //this.tapTarget.destroy()
+  }
+
+  openInfo(): void{
+    //this.tapTarget.open()
+  }
 }
+
--- client/src/app/shared/interfaces.ts
+++ client/src/app/shared/interfaces.ts
@@ -46,3 +46,15 @@ export interface Filter {
   end?: Date
   order?: number
 }
+
+export interface OverviewPage {
+  orders: OverviewPageItem
+  gain: OverviewPageItem
+}
+
+export interface OverviewPageItem {
+  percent: number
+  compare: number
+  yesterday: number
+  isHigher: boolean
+}
